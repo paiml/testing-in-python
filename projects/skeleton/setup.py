f62f85e04e5b82f04f2bdc070e8ac8d50247713f
Alfredo Deza <alfredo@deza.pe>
2020-01-05 12:03:26 -0500

    create a projects directory

---
+++ projects/skeleton/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='skeleton',
+    version='0.1',
+    description='',
+    author='',
+    author_email='',
+    packages=find_packages()
+)
