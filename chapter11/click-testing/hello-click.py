992e03a9e2173dc0d2af1692070e9b35c2d1c064
Noah Gift <noah.gift@gmail.com>
2020-01-22 05:03:12 -0500

    updating click cli code

--- chapter11/click-testing/hello-click.py
+++ chapter11/click-testing/hello-click.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import click
 
+
 @click.command()
 def hello():
-    click.echo('Hello World!')
+    click.echo("Hello World!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     hello()
